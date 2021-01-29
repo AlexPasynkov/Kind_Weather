1d2862adec9dbe364c24e396972f400a98fb3d11
AlexPasynkov <54889913+AlexPasynkov@users.noreply.github.com>
2021-01-29 16:47:55 +0100

    Initial commit

---
+++ app/src/main/java/com/alexlearn/kindweather/ui/activities/MainActivity.java
@@ -0,0 +1,35 @@
+package com.alexlearn.kindweather.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.alexlearn.kindweather.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.bottomNavView);
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home,
+                R.id.navigation_history
+        ).build();
+
+        NavController navController = Navigation.findNavController(this, R.id.navHostFragment);
+        NavigationUI.setupActionBarWithNavController(this,  navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+
+
+    }
+}
---
+++ app/src/main/java/com/alexlearn/kindweather/util/Constants.java
@@ -0,0 +1,10 @@
+package com.alexlearn.kindweather.util;
+
+public class Constants {
+
+    public static final String URL = "http://api.openweathermap.org/data/2.5/";
+    public static final String API_KEY = "d869a94743e465459abf929e018ee142";
+
+    // This api by default sent data in Imperial measurements (F)
+    public static final String UNITS_METRIC = "metric";
+}
