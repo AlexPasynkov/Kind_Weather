9a54c2ba5cca0ce964f0cc0cd6b5a73749150039
AlexPasynkov <54889913+AlexPasynkov@users.noreply.github.com>
2021-02-02 14:40:48 +0100

    Update Constants.java

--- app/src/main/java/com/alexlearn/kindweather/util/Constants.java
+++ app/src/main/java/com/alexlearn/kindweather/util/Constants.java
@@ -3,7 +3,7 @@ package com.alexlearn.kindweather.util;
 public class Constants {
 
     public static final String URL = "http://api.openweathermap.org/data/2.5/";
-    public static final String API_KEY = "d869a94743e465459abf929e018ee142";
+    public static final String API_KEY = "";
 
     // This api by default sent data in Imperial measurements (F)
     public static final String UNITS_METRIC = "metric";
